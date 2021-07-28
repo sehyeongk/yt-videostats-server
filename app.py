81dc5c993bcdc55a42e811d47e69631393e21210
sehyeongkim <sehyeongkim@sandboxnetwork.net>
2021-07-28 17:35:21 +0900

    Add: Search Video api 작성

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
-from controller import get_videos_list, get_videos_info
+from controller import search_video_by_title, get_videos_info
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.add_url_rule('/videos', view_func=get_videos_list, methods=['GET'])
+    app.add_url_rule('/videos', view_func=search_video_by_title, methods=['GET'])
     app.add_url_rule('/video', view_func=get_videos_info, methods=['GET'])
 
     return app
--- controller.py
+++ controller.py
@@ -1,9 +1,44 @@
-from database import engine
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from flask import request, jsonify
+from database import engine
+from models import Video
 
-def get_videos_list():
-    pass
+
+def search_video_by_title():
+    try:
+        title = request.args.get('title', None)
+        if not title:
+            return jsonify('Enter the title to search for'), 400
+
+        statement = select(Video).filter(Video.title.like(f'%{title}%')).limit(20)
+        with Session(engine) as session:
+            videos = session.execute(statement)
+
+        result = []
+        for row in videos:
+            video = row['Video']
+            result.append(
+                {
+                    'id': video.id,
+                    'channel_id': video.channel_id,
+                    'title': video.title,
+                    'status': video.status,
+                    'description': video.description,
+                    'thumbnail': video.thumbnail,
+                    'published_at': video.published_at,
+                    'created_at': video.created_at,
+                    'updated_at': video.updated_at,
+                    'deleted_at': video.deleted_at
+                }
+            )
+
+        return jsonify(result), 200
+
+    except Exception as e:
+        return jsonify(e), 400
+
 
 def get_videos_info():
     pass
